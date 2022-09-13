e3f965039187e1458eac2319a01e27c49d4a7c38
Avihail <stepovoyas@gmail.com>
2022-09-13 19:05:26 +0300

    few solutions

---
+++ String ends with.js
@@ -0,0 +1,19 @@
+// Task: Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//
+// Examples:
+//
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+
+//Solution #1
+function solution(str, ending){
+    return ending === str.substr(str.length - ending.length);
+}
+
+//Solution #2
+function solution(str, ending){
+    return str.endsWith(ending);
+}
+
+//shorter #2
